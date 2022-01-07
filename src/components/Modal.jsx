b39428ef528ba123111b29ac48070ebfcf6649e1
= <it7mdx@googlemail.com>
2022-01-07 21:33:44 +0300

    allow caching

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,7 @@ function Modal({url, open, close, setLoaded, imageLoading}){
           {imageLoading && <div className="d-flex justify-content-center p-3">
                         <svg width="1.6em" height="1.6em" viewBox="0 0 24 24" id="rotate"><path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 18a8 8 0 1 1 8-8a8 8 0 0 1-8 8z" opacity=".5" fill="#744867"></path><path d="M20 12h2A10 10 0 0 0 12 2v2a8 8 0 0 1 8 8z" fill="#341436"></path></svg>
           </div>}
-            <img onLoad={()=>setLoaded(false)} src={url + "?" + Math.random()} />
+            <img onLoad={()=>setLoaded(false)} src={url} />
         </article>
       </dialog>
             )
